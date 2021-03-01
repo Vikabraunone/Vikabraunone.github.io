41fe3a78f50a72f879f7aef3d168ce0e747eeec9
Vika <vikabraunone@gmail.com>
2021-03-01 22:00:46 +0400

    lab5

---
+++ js/site.js
@@ -0,0 +1,56 @@
+const form = document.forms[0];
+const buttonsToBasket = document.getElementsByName("addBasketButton");
+
+buttonsToBasket.forEach(function(entry) {
+  entry.addEventListener('click', event =>
+  {
+    Swal.fire({
+      icon: 'success',
+      title:'Товар добавлен в корзину'
+    });
+  }
+)});
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  new FormData(form);
+});
+
+form.addEventListener("formdata", event => {
+  const data = event.formData;
+
+  const entries = [...data.entries()];
+  console.log(entries);
+
+  const values = [...data.values()];
+  console.log(values);
+
+  let index = values[3];
+  if (isNaN(index))
+  {
+    Swal.fire({
+      icon: 'error',
+      title:'Неверный индекс!'
+    });
+    return;
+  }
+
+  if (index < 100000 || index > 999999)
+  {
+    Swal.fire({
+      icon: 'error',
+      title:'Неверный индекс!'
+    });
+    return;
+  }
+
+  Swal.fire(
+    'Заказ оформлен!',
+    'Ждите звонка от нашего менеджера',
+    'success'
+  );
+});
+
+function getradiobuttons(){
+  alert("selected: " + $('input[name=radio-stacked]:checked').val());
+}
